6fae60ea3d5051a7fcc705efb357cef3a84bf290
Tin Mac <tin@Tinui-noteubug.local>
2024-03-16 18:26:38 +0900

    백오피스API활용 운영자동화 / 티켓자동발행

---
+++ backoffice_API/insert_ticket_automation/Main.py
@@ -0,0 +1,10 @@
+from fn_insert_ticket import v3_insert_ticket
+
+v3_insert_ticket(
+    "sheet_id" ## 구글 시트 아이디 입력
+    ,"google_sheet_key" ## 구글 API 키 입력
+    ,"sheet_name"
+    ,"python_upload_sheet_name"
+    ,"slack_channel_id"
+)
+
